26cd2b7a1cc5e9888072de639d2ced87ebda1d8f
Andres Enrique Jiménez Mogollón <andresejimenez@hotmail.com>
2022-05-28 18:09:30 -0500

    Add project files.

---
+++ TryAndCatch/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace TryAndCatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Enter a number: ");
+                int number1 = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Enter another number please: ");
+                int number2 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine(number1 / number2);
+            }
+            
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadLine();
+        }
+
+    }
+    
+}
